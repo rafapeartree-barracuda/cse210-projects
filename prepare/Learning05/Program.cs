782867400cc1614690fc15140154e6c807c42496
Rafael Roberto <rafael.roberto@supportcomm.com.br>
2024-10-14 18:14:02 -0300

    Inheritance Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment student1 = new Assignment("Rafael Roberto", "Programming with Classes");
+        Console.WriteLine(student1.GetSummary());
+
+        MathAssignment student2 = new MathAssignment("Roberto Pereira", "Fractions", "7.3", "8-19");
+        Console.WriteLine(student2.GetSummary());
+        Console.WriteLine(student2.GetHomeworkList());
+
+        WritingAssignment student3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(student3.GetSummary());
+        Console.WriteLine(student3.GetWritingInformation());
     }
 }
