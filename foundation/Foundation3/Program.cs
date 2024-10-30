e7423985fda208946362ecfe334ba440a543e9a4
Rafael Roberto <rafael.roberto@supportcomm.com.br>
2024-10-30 00:47:32 -0300

    W07 Prove: Final Foundation—Inheritance and Polymorphism

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 40, 25.0),
+            new Swimming(new DateTime(2022, 11, 3), 45, 30)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
